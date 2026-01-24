f70094c19e787491a4561a9d813c72e50be13530
Nikitos <krasilnikovnikita11@gmail.com>
2026-01-24 14:23:46 +0300

    24.01.26
    fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,18 @@
-import { withAuth } from "next-auth/middleware"
+import { withAuth } from 'next-auth/middleware'
 
 export default withAuth({
   pages: {
-    signIn: "/admin/login",
+    signIn: '/admin/login',
+  },
+  callbacks: {
+    authorized: ({ token, req }) => {
+      const pathname = req.nextUrl.pathname
+      if (pathname === '/admin/login') return true
+      return Boolean(token)
+    },
   },
 })
 
 export const config = {
-  matcher: ["/admin((?!/login).*)"],
+  matcher: ['/admin/:path*'],
 }
