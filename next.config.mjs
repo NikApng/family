3a872c166ac9d09c0fc3aa3500724f3b38be916a
Nikitos <krasilnikovnikita11@gmail.com>
2026-01-24 14:15:00 +0300

    24.01.26  fix nex.config

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'www.canva.com' },
+    ],
+  },
+}
+
+export default nextConfig
